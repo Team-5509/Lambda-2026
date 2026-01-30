8cd72ec6f998c7c5c2a9ee3b4e5360ca63ea83f6
Paxton Hilliard <pnh705@gmail.com>
2026-01-29 19:43:02 -0500

    fixed build issues twice, renamed example subsystem class,
    Co-Authored-By: MrRobinsonBot <256225171+MrRobinsonBot@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,16 +9,17 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ConveyorSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ConveyorSubsystem() {}
-private TalonFX conveyorMotor=new TalonFX(15);
+  public IntakeSubsystem() {}
+    private TalonFX intakeMotor = new TalonFX(4);
+
   /**
    * Example command factory method.
    *
    * @return a command
    */
-  public Command ConveyorMethodCommand() {
+  public Command exampleMethodCommand() {
     // Inline construction of command goes here.
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(
@@ -32,7 +33,7 @@ private TalonFX conveyorMotor=new TalonFX(15);
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
-  public boolean ConveyorCondition() {
+  public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
   }
@@ -46,12 +47,10 @@ private TalonFX conveyorMotor=new TalonFX(15);
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
-  public void SetConveyorMotor(double speed){
-    conveyorMotor.set(speed);
+  public void setIntakeMotor(double speed) {
+    intakeMotor.set(speed);
   }
-public void stopconveyormotor(){
-conveyorMotor.set(0);
-}
-
-  
+  public void stopIntakeMotor() {
+    intakeMotor.set(0);
+  }
 }
