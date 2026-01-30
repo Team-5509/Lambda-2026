d7e6911ff3da524cf1ff1087ccd2ed594d1ed50f
MrRobinsonBot <jcswanton11@gmail.com>
2026-01-29 20:31:43 -0500

    Made conveyor command file, added speed command, added conveyor execute and conveyor end command

--- src/main/java/frc/robot/commands/ConveyorCommand.java
+++ src/main/java/frc/robot/commands/ConveyorCommand.java
@@ -3,21 +3,23 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ConveyorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
+public class ConveyorCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final ConveyorSubsystem m_subsystem;
+  private final Double m_speed;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
+  public ConveyorCommand(ConveyorSubsystem subsystem, double speed ) {
     m_subsystem = subsystem;
+    m_speed = speed;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
@@ -28,12 +30,17 @@ public class ExampleCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_subsystem.SetConveyorMotor(m_speed);
+  }
+   
+  
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
+  public void end(boolean interrupted) {
+m_subsystem.SetConveyorMotor(0);
+  }
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
